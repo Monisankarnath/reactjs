127173fef681cf95ef9fb90fa323c67b83e2788b
Monisankar <31494899+Monisankarnath@users.noreply.github.com>
2020-08-24 15:45:02 +0530

    Add files via upload

---
+++ news_app/build/precache-manifest.dd6215515b2005a78e86b8a0532a6a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645d7c20ff2a07aab73042160f9c0945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90efa485cf99a78dcce",
+    "url": "/static/css/main.1641f3b2.chunk.css"
+  },
+  {
+    "revision": "01b78d273ad3e1f2f015",
+    "url": "/static/js/2.56c922a7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.56c922a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90efa485cf99a78dcce",
+    "url": "/static/js/main.2370f89c.chunk.js"
+  },
+  {
+    "revision": "fae5941e4e5d78cbc530",
+    "url": "/static/js/runtime-main.4529b8ec.js"
+  }
+]);
